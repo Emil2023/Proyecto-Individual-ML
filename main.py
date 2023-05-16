f389d5646672937464f474c94ae24bcff2d92c02
Emilioo <117398575+Emil2023@users.noreply.github.com>
2023-05-16 13:37:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ import asyncio
 
 
 #Funcion para el usario ingrese el mes y dia en español
-app = FastAPI()
+app = FastAPI(
+    title = "Bienvenido  a Los datos e informacion de la empresa sobre peliculas, series y productoras",
+    description = "Datos",
+    )
 
 df = pd.read_csv('clean_movies_dataset.csv')
 df['release_date'] = pd.to_datetime(df['release_date'])
@@ -19,13 +22,27 @@ df['release_month'] = df['release_date'].dt.month_name()
 df['release_day'] = df['release_date'].dt.day_name()
 
 def traductor_mes():
-    meses = {'January':'Enero', 'February':'Febrero', 'March':'Marzo', 'April':'Abril', 'May':'Mayo', 'June':'Junio', 'July':'Julio', 'August':'Agosto', 'September':'Septiembre', 'October':'Octubre', 'November':'Noviembre', 'December':'Diciembre'}
+    meses = {
+        "enero": "january",
+        "febrero": "february",
+        "marzo": "march",
+        "abril": "april",
+        "mayo": "may",
+        "junio": "june",
+        "julio": "july",
+        "agosto": "august",
+        "septiembre": "september",
+        "octubre": "october",
+        "noviembre": "november",
+        "diciembre": "december"}
+    
     df['release_month'] = df['release_month'].map(meses)
 traductor_mes()
 def traductor_dia():
     dias = {'Monday':'Lunes', 'Tuesday':'Martes', 'Wednesday':'Miercoles', 'Thursday':'Jueves', 'Friday':'Viernes', 'Saturday':'Sabado', 'Sunday':'Domingo'}
     df['release_day'] = df['release_day'].map(dias)
 traductor_dia()
+
 df['release_date'] = pd.to_datetime(df['release_date'])
 df['release_month'] = df['release_date'].dt.month_name()
 df['release_year'] = df['release_year'].astype(str)
@@ -83,20 +100,49 @@ def retorno(pelicula):
 #ML                                        
 @app.get("/ml_movie/{pelicula}")
 def ml_movie(selected_title):
-    df = pd.read_csv('clean_movies_dataset.csv') 
-    k = 6
-    generos_df = pd.read_csv('genre_binario.csv', index_col=0).astype('float32')
-    selected_genres = df.loc[df['title'] == selected_title]['genre_names'].values[0]
-    df['genre_similarity'] = df['genre_names'].apply(lambda x: len(set(selected_genres) & set(x)) / len(set(selected_genres) | set(x)))
-    df['same_series'] = df['title'].apply(lambda x: 1 if 'Batman' in x else 0)
+    df = pd.read_csv('clean_movies_dataset.csv')
+    print(df['title'].head())
+    df['tagline'] = df['tagline'].fillna('')
+   
+    df = df.dropna(subset=['overview', 'tagline', 'genre_names', 'title', 'id'])
+
+    generos_df = df['genre_names'].str.join(sep='|').str.get_dummies()
+    
+    selected_genres = df.loc[df['title'] == selected_title]['genre_names'].values
+    if len(selected_genres) == 0:
+        return "No se encontró la película " + selected_title
+    selected_genres = ast.literal_eval(selected_genres[0])
+    df['genre_similarity'] = df['genre_names'].apply(lambda x: len(set(selected_genres) & set(ast.literal_eval(x))) / len(set(selected_genres) | set(ast.literal_eval(x))))
+   
+    df['same_series'] = df['title'].apply(lambda x: 1 if selected_title.lower() in x.lower() else 0)
+
     features_df = pd.concat([generos_df, df['vote_average'], df['genre_similarity'], df['same_series']], axis=1)
+
+    k = 6
     knn = NearestNeighbors(n_neighbors=k+1, algorithm='auto')
     knn.fit(features_df)
     indices = knn.kneighbors(features_df.loc[df['title'] == selected_title])[1].flatten()
-    movies = list(df.iloc[indices]['title'])
-    movies = sorted(movies, key=lambda x: (df.loc[df['title'] == x]['same_series'].values[0], df.loc[df['title'] == x]['vote_average'].values[0], df.loc[df['title'] == x]['genre_similarity'].values[0]), reverse=True)
-    movies = [movie for movie in movies if movie != selected_title]
-    return movies[0:5]                                                           
+    recommended_movies = list(df.iloc[indices]['title'])
+
+
+    selected_score = df.loc[df['title'] == selected_title]['vote_average'].values[0]
+    recommended_movies = sorted(recommended_movies, key=lambda x: (df.loc[df['title'] == x]['same_series'].values[0], df.loc[df['title'] == x]['vote_average'].values[0], df.loc[df['title'] == x]['genre_similarity'].values[0]), reverse=True)
+    recommended_movies = [movie for movie in recommended_movies if movie != selected_title]
+
+ 
+    if len(recommended_movies) == 0:
+        return "No se encontraron películas similares a " + selected_title
+    else:
+        respuesta = f"Película seleccionada: {selected_title} ({selected_score}):\n\nPelículas Recomendadas:\n"
+        for i, pelicula in enumerate(recommended_movies[:5]):
+            score = df.loc[df['title'] == pelicula]['vote_average'].values[0]
+            genres = df.loc[df['title'] == pelicula]['genre_names'].values[0]
+            gen_str = ', '.join(ast.literal_eval(genres))
+
+            respuesta += f"-{pelicula}  | Géneros: {gen_str} | Puntaje: {score} |\n"
+            if i == 4:
+                break
+        return respuesta                 
                                         
                                         
                                         
